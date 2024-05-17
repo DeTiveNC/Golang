e5eb8d1e5b819c820c2a47e2a9480dba536251a0
DeTiveNC <116792124+DeTiveNC@users.noreply.github.com>
2024-05-17 13:05:30 +0200

    Golang : Changes on passwordGen

--- passwordGen/cmd/root.go
+++ passwordGen/cmd/root.go
@@ -1,7 +1,5 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2024 DeTiveNC detivenc@proton.me
 
-*/
 package cmd
 
 import (
@@ -9,8 +7,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -47,5 +43,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- passwordGen/go.mod
+++ passwordGen/go.mod
@@ -1,6 +1,6 @@
 module github.com/detivenc/passwordGen
 
-go 1.22.0
+go 1.22.3
 
 require github.com/spf13/cobra v1.8.0
 
